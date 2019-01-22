ff0140b756baaf41560c3cd28b056872a540a9e8
Astralof <41782505+Astralof@users.noreply.github.com>
2019-01-22 16:43:19 +0100

    Add comments

--- db/migrate/20190122141652_create_comments.rb
+++ db/migrate/20190122141652_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
     create_table :comments do |t|
       t.string :author_name
       t.text :body
-      t.references :article, foreign_key: true
+      t.references :article
 
       t.timestamps
     end
